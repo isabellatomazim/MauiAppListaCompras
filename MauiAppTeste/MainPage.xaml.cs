08716b14717a5789df5fd62dd6e23407ba8432bc
Usuario <Usuario@CPD03-M11>
2024-03-28 15:09:34 -0300

    avanço

--- MauiAppTeste/MainPage.xaml.cs
+++ MauiAppTeste/MainPage.xaml.cs
@@ -49,18 +49,18 @@ namespace MauiAppTeste
             await Navigation.PushAsync(new Views.NovoProduto());
         }
 
-        private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
         {
             string q = e.NewTextValue;
             Lista_Produtos.Clear();
-            Task.Run(async () =>
-            {
+          
+            
                 List<Produto> tmp = await App.Db.Search(q);
                 foreach (Produto p in tmp)
                 {
                     Lista_Produtos.Add(p);
                 }
-            });
+            
         }
 
         private void ref_carregando_Refreshing(object sender, EventArgs e)
@@ -75,7 +75,7 @@ namespace MauiAppTeste
                 }
 
             });
-            ref_carregando.IsRefreshing = false;
+            
         }
 
         private void list_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -102,6 +102,8 @@ namespace MauiAppTeste
                 {
                     await App.Db.Delete(p.Id);
                     await DisplayAlert("Sucesso!", "Produto Removido", "OK");
+                    Lista_Produtos.Remove(p);
+
                 }
             }catch (Exception ex)
             {
